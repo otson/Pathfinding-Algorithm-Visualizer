12b6a7dcf953521e813ee8622401d2ea9ef180ef
otson <59139705+otson@users.noreply.github.com>
2021-10-02 18:08:27 +0300

    Add cell component, remove unnecessary files

--- frontend/searchalgorithmvisualizer/src/app/app.module.ts
+++ frontend/searchalgorithmvisualizer/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule
