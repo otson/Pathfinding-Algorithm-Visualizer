6c0c44a18641c3c4dacb95ad3016ad0eb48195c5
otson <59139705+otson@users.noreply.github.com>
2021-10-02 17:57:56 +0300

    move backend folder

---
+++ backend/src/main/java/fi/nuortimo/searchalgorithmvisualizer/SearchalgorithmvisualizerApplication.java
@@ -0,0 +1,13 @@
+package fi.nuortimo.searchalgorithmvisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchalgorithmvisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchalgorithmvisualizerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fi/nuortimo/searchalgorithmvisualizer/SearchalgorithmvisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.nuortimo.searchalgorithmvisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchalgorithmvisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
